234366a60dd1490937ad27bd4cca453ed50adfa0
Aizen_o9 <scilentgamer99@gmail.com>
2025-07-03 23:35:56 +0530

    Successfully implemented the relationship betwween entities part

--- BACKEND/models/orderModel.js
+++ BACKEND/models/orderModel.js
@@ -1,41 +1,55 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    userId:{
-        type:String,
-        required : true
-    },
-    items:{
-        type:Array,
-        required : true
-    },
-    amount:{
-        type:Number,
-        required : true
-    },
-    address:{
-        type : Object,
-        required : true
-    },
-    status:{
-        type:String,
-        required : true,
-        default : 'Order Placed'
-    },
-    paymentMethod:{
-        type:String,
-        required : true
-    },
-    payment:{
-        type:Boolean,
-        required : true,
-        default:false
-    },
-    Date:{
-        type:Number,
-        required : true,
-    },
-})
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // this should be same as the one in Product
+    required: true
+  },
+  items: [
+    {
+      //Product Relationship 
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product", 
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      },
+      size: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  amount: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: Object,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'Order Placed',
+    required: true
+  },
+  paymentMethod: {
+    type: String,
+    required: true
+  },
+  payment: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  Date: {
+    type: Number,
+    required: true
+  }
+});
 
-
-module.exports = mongoose.model('order',orderSchema);
+module.exports = mongoose.model('Order', orderSchema);
